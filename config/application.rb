e1cea5b17407cff52725afb03526e226e3e19b08
Ryan Mont <ryanmont@gmail.com>
2019-05-31 16:39:27 -0600

    added time structure to show created at

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Ryanmont
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    
+    config.time_zone = "America/Denver"
 
     config.autoload_paths << Rails.root.join("lib")
     config.generators.system_tests = nil
